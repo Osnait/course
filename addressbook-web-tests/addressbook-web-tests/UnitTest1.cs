81982d53511f572429f27961505e9e20f4bfc1b2
osnait <osnait@yandex.ru>
2023-06-03 12:11:04 +0700

    test for creating a group of contacts

--- addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -1,10 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace addressbook_web_tests
 {
-    [TestClass]
-    public class UnitTest1
+   [TestClass]
+   public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+     [TestMethod]
+      public void TestMethod1()
         {
         }
     }
