43230b65dabe772859cadd425a2d46479b8850a5
osnait <osnait@yandex.ru>
2023-06-22 02:56:57 +0700

    Classes and object

---
+++ addressbook-web-test/addressbook-web-test/Figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_test
+{
+    internal class Figure
+    {
+        public bool colored = false;
+
+        public bool Colored
+        {
+            get
+            {
+                return colored;
+            }
+
+            set
+            {
+                colored = value;
+            }
+        }
+    }
+}
